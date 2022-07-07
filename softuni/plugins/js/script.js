39e131fa5c8ff2376cdff22ac042ff1c2c55b24d
Nikolay Dimov <nikolay.dimov83@gmail.com>
2022-07-07 08:23:56 +0300

    Show -hide logic ready

--- softuni/plugins/js/script.js
+++ softuni/plugins/js/script.js
@@ -1,15 +1,17 @@
 $(function() {
     
     $.fn.showHide=function (){
-        $(`.hide-show`).each(function(index,element){
-            element.childrenStatus=false;
+        $(`input`).each(function(index,element){
+            element.childrenStatus=true;
+            console.log(element.value)
             $(this).on('click',ev=>{
-                    if(!element.childrenStatus){
-                    element.childrenStatus=true;
-                    $(this).children().ccs('display:list-item')
+                    if(element.childrenStatus){
+                    element.childrenStatus=false;
+                    $(this).next().css('visibility','hidden')
+                    
                 }else{
-                    element.childrenStatus=false
-                    $(this).children().ccs('display:none')
+                    element.childrenStatus=true
+                    $(this).next().css('visibility','visible')
                 }
             })
             element.addEventListener('click',ev=>{
