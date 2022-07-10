cb03f06da23580bb7a9d4aec7e89ad4cf669993d
Nikolay Dimov <nikolay.dimov83@gmail.com>
2022-07-10 07:56:35 +0300

    Update message.js

--- softuni/plugins/js/message.js
+++ softuni/plugins/js/message.js
@@ -1,15 +1,16 @@
 $(function() {
     $.fn.messageBox=function(){
         return {
+            whoCalled:this,
             success:success,
             error:error
 
         }
         function success(message){
-            $(this).text(message).fadeIn(1000).hide(3000);
+            $(this.whoCalled).text(message).fadeIn(1000).hide(3000);
         }
         function error(message){
-            $(this).text(message).fadeIn(1000).hide(3000);
+            $(this.whoCalled).text(message).fadeIn(1000).hide(3000);
         }    
     }
 
